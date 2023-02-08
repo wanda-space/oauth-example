12b0a9b91ec3f419d389b0b5274b9c2ae2a64fe9
joshua-schecker <joshua@wanda.space>
2023-02-08 10:59:30 +0100

    Add logout button and scopes to login request

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ function App() {
   console.log(process.env)    
   const auth0 = new WebAuth({
     domain: 'id-dev.wanda.space',
-    redirectUri: process.env.REACT_APP_REDIRECT_URL,
     responseType: 'code',
     clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
   });
   const params = new URLSearchParams(window.location.search);
   const code = params.get('code');
+  const loginOptions = {    
+    scope: "write:items read:items write:orders read:orders",
+    prompt: "consent",
+  redirectUri: process.env.REACT_APP_REDIRECT_URL ??'https://wanda-space.github.io/oauth-example',
+
+}
+const logoutOptions = {returnTo: process.env.REACT_APP_REDIRECT_URL ??'https://wanda-space.github.io/oauth-example'}
   return (
     <div className="App">
-      <header className="App-header"><button onClick={()=>auth0.authorize()}>log in</button>{code && <p>Auth code:<br />{code}</p>}</header>
+      <header className="App-header"><button onClick={()=>auth0.authorize(loginOptions)}>log in</button><button onClick={()=>auth0.logout(logoutOptions)}>log out</button>{code && <p>Auth code:<br />{code}</p>}</header>
     </div>
 
   );
