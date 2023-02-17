e82e3f0e7e922494d95ed06d80080e878ee1eedd
joshua-schecker <joshua@wanda.space>
2023-02-17 11:02:30 +0100

    add token request example

--- src/App.js
+++ src/App.js
@@ -20,7 +20,18 @@ function App() {
 const logoutOptions = {returnTo: process.env.REACT_APP_REDIRECT_URL ??'https://wanda-space.github.io/oauth-example'}
   return (
     <div className="App">
-      <header className="App-header"><button onClick={()=>auth0.authorize(loginOptions)}>log in</button><button onClick={()=>auth0.logout(logoutOptions)}>log out</button>{code && <p>Auth code:<br />{code}</p>}</header>
+      <header className="App-header">
+        <button onClick={()=>auth0.authorize(loginOptions)}>log in</button>
+        <button onClick={()=>auth0.logout(logoutOptions)}>log out</button>
+      {code && <p className="code">Auth code:<br />{code}</p>}
+      </header>
+      {code && <code>
+        {`curl --request POST \\`}<br/>
+ { `--url 'https://id-dev.wanda.space/oauth/token' \\`}<br/>
+  {`--header 'content-type: application/x-www-form-urlencoded' \\`}<br/>
+ { `--data 'grant_type=authorization_code&client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&client_secret=YOUR_CLIENT_SECRET&code=${code}&redirect_uri={https://yourApp/callback}'
+`}
+      </code>}
     </div>
 
   );
